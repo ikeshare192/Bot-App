789dfe18e0f10754173f4d3d5b249728d00f8c86
Isaac Shareef <64051998+ikeshare192@users.noreply.github.com>
2021-09-15 22:53:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,10 +61,5 @@ def processRequest(req):
             "fulfillmentText":fulfillmentText
         }
         
-#if __name__ == "__main__":
-    #app.run()
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run()
