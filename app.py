61f352ec11622cc3716c903a998ae8390aaca79d
ikeshare192 <isaacshareef@hotmail.com>
2021-09-21 17:22:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -66,11 +66,3 @@ def webhook():
 
 if __name__ == "__main__":
     app.run()
-
-'''
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
-
-'''
