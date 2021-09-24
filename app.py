4d8fb6be8372c36fbed7294f968535bba4e227b7
Isaac Shareef <64051998+ikeshare192@users.noreply.github.com>
2021-09-23 20:44:22 -0500

    remove flask_cors

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import numpy as np
 from flask import Flask, request, make_response
 import json
 import pickle
-from flask_cors import cross_origin 
 
 app = Flask(__name__)
 model = pickle.load(open('model', 'rb'))
